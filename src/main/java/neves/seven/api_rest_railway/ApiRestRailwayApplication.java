65c13625bd111cea079ac4623822284044b72dd6
abominavelneves <pythonproject911@gmail.com>
2024-10-04 10:47:23 -0300

    Início do Projeto -Api Restful usando Java e Spring

---
+++ src/main/java/neves/seven/api_rest_railway/ApiRestRailwayApplication.java
@@ -0,0 +1,13 @@
+package neves.seven.api_rest_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neves/seven/api_rest_railway/ApiRestRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package neves.seven.api_rest_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
